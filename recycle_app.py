403705ba43d66cc0114cc7b385311b99026d3c58
mary-mwangi <43517252+mary-mwangi@users.noreply.github.com>
2021-05-06 15:39:25 +0200

    Update recycle_app.py

--- recycle_app.py
+++ recycle_app.py
@@ -31,7 +31,7 @@ if uploaded_file is not None:
 
     uploaded_image = preprocess_image(uploaded_image)
 
-    if st.button("Predict:"):
+    if st.button("Classify:"):
         pred = model.predict(uploaded_image)
         label = np.argmax(pred, axis=1)
         item = class_names[label[0]]
@@ -40,3 +40,28 @@ if uploaded_file is not None:
             st.write("This item is " + item  + ". Your waste material is not recylable")
         else:
             st.write("This item is " + item  + ". Your waste material is recylable")
+
+st. subheader("Quick Facts")
+st.write("Glass is 100% recyclable! As it is easy to wash off contaminations, it is the most likely to be recycled multiple times. Simply ensure to not break into tiny pieces")
+st.write("To go cups are not recyclable. Do not confuse them with paper or cardboard ")
+st.write("Water is a contaminant. Remember to close your bin in case of stormy weather.")
+
+
+st. subheader("Useful Links")
+
+st.write("Recycling 101")
+col1, mid, col2 = st.beta_columns([1,1,20])
+with col1:
+    st.image('row_2_col_2.jpg', width=60)
+with col2:
+    st.write('https://www.youtube.com/watch?v=kozJmBpLDF8')
+
+
+st.write("How to Properly Reycle At Home")
+col1, mid, col2 = st.beta_columns([1,1,20])
+with col1:
+    st.image('row_2_col_1.jpg', width=60)
+with col2:
+    st.write('https://www.youtube.com/watch?v=jsp7mgYv3aI')
+
+
